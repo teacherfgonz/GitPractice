d1afd98d5150cc7bb9ef64ff8be7f9617f6c9d71
rjhurry <hurryroberto@gmail.com>
2024-05-03 09:41:48 -0300

    add info to test case and change test data const name to something that is not generic

--- tests/roberto.spec.ts
+++ tests/roberto.spec.ts
@@ -3,7 +3,7 @@ import { test, expect, type Page } from '@playwright/test';
 const checkbox1 = (page: Page) => page.getByRole('checkbox').nth(0);
 const checkbox2 = (page: Page) => page.getByRole('checkbox').nth(1);
 
-const testCases = {
+const testCheckboxesData = {
   checked: {
     checked: true,
   },
@@ -12,28 +12,39 @@ const testCases = {
   },
 };
 
-test.describe('Test Checkboxes', { tag: '@Regression' }, () => {
-  for (const state of Object.keys(testCases)) {
-    test(`Test Checkboxes all ${state}`, async ({ page }) => {
-      await test.step('Go to the Checkboxes page', async () => {
-        await page.goto('/');
-        await page.getByRole('link', { name: 'Checkboxes' }).click();
-      });
+test.describe(
+  'Test Checkboxes',
+  {
+    tag: '@Regression',
+    annotation: {
+      type: 'Info',
+      description:
+        'The page loads with the first checkbox unchecked and second checked',
+    },
+  },
+  () => {
+    for (const state of Object.keys(testCheckboxesData)) {
+      test(`Test Checkboxes all ${state}`, async ({ page }) => {
+        await test.step('Go to the Checkboxes page', async () => {
+          await page.goto('/');
+          await page.getByRole('link', { name: 'Checkboxes' }).click();
+        });
 
-      await test.step('Set checkbox 1', async () => {
-        await checkbox1(page).setChecked(testCases[state]['checked']);
-      });
+        await test.step('Set checkbox 1', async () => {
+          await checkbox1(page).setChecked(testCheckboxesData[state]['checked']);
+        });
 
-      await test.step('Set checkbox 2', async () => {
-        await checkbox2(page).setChecked(testCases[state]['checked']);
-      });
+        await test.step('Set checkbox 2', async () => {
+          await checkbox2(page).setChecked(testCheckboxesData[state]['checked']);
+        });
 
-      await expect
-        .soft(checkbox1(page), `Verified checkbox 1 is ${state}`)
-        .toBeChecked({ checked: testCases[state]['checked'] });
-      await expect
-        .soft(checkbox2(page), `Verified checkbox 2 is ${state}`)
-        .toBeChecked({ checked: testCases[state]['checked'] });
-    });
+        await expect
+          .soft(checkbox1(page), `Verified checkbox 1 is ${state}`)
+          .toBeChecked({ checked: testCheckboxesData[state]['checked'] });
+        await expect
+          .soft(checkbox2(page), `Verified checkbox 2 is ${state}`)
+          .toBeChecked({ checked: testCheckboxesData[state]['checked'] });
+      });
+    }
   }
-});
+);
