328b27d483054c8e17b770e3298e0712c6d37bcb
teacherfgonz <teacherfranciscogonz@gmail.com>
2024-05-07 11:30:46 -0300

    deleting test case that deletes element from last to first.

--- tests/francisco.spec.ts
+++ tests/francisco.spec.ts
@@ -8,22 +8,14 @@ const testCasesAddRemove = {
   'User adds elements and remove them all': {
     add: 20,
     remove: 20,
-    firstToLast: true,
   },
   'User adds 10 elements and remove 5': {
     add: 10,
     remove: 5,
-    firstToLast: true,
-  },
-  'User adds 50 elements and remove them all from last to first': {
-    add: 50,
-    remove: 50,
-    firstToLast: false,
   },
   'User adds 30 elements and remove none': {
     add: 30,
     remove: 0,
-    firstToLast: true,
   },
 };
 
@@ -50,15 +42,9 @@ test.describe(
         });
 
         await test.step(`Delete ${testCasesAddRemove[testCases].remove} added elements`, async () => {
-          if (testCasesAddRemove[testCases].firstToLast) {
-            await deleteButton(page)
-              .nth(0)
-              .click({ clickCount: testCasesAddRemove[testCases].remove });
-          } else {
-            for (let i = testCasesAddRemove[testCases].remove - 1; i > -1; i--) {
-              await deleteButton(page).nth(i).click();
-            }
-          }
+          await deleteButton(page)
+            .nth(0)
+            .click({ clickCount: testCasesAddRemove[testCases].remove });
         });
 
         const deleteBtnNumber =
